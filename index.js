349b50f62d36edacea720cf7fbfdd52e7e8fb69f
Gabriele Armento <gabriele.armento@gmail.com>
2023-05-31 18:08:16 +0200

    Add offers

--- index.js
+++ index.js
@@ -3,36 +3,58 @@
 // If we need to access data in the current repo, we can use this
 const BASE_URL = "https://965311532.github.io/genoleggia.it";
 
-// When the DOM is loaded, initialize the router
-document.addEventListener("DOMContentLoaded", function (event) {
+// Don't wait for the DOM to be ready, start loading data immediately
+function start() {
   const currentPath = window.location.pathname;
   console.debug("Current path: " + currentPath);
   switch (currentPath) {
     case "/":
-      console.debug("Loading home page");
       loadHomePageOffers();
   }
-});
+}
+
+// getElementById but wait until the DOM is ready
+function getElementById(id) {
+  while (true) {
+    if (document.readyState !== "loading") {
+      return document.getElementById(id);
+    }
+    sleep(25); // Pause for a small amount of time
+  }
+}
 
 // Function to load the offers on the home page
 function loadHomePageOffers() {
-  const target = document.getElementById("hero-offers");
-  console.debug("Target element: " + target);
-  const imageUrl =
-    "https://cdn-08.imagin.studio/getImage?&customer=itgenoleggia-spa&make=lancia&modelFamily=ypsilon&modelRange=ypsilon&modelVariant=ha&modelYear=2018&powerTrain=fossil&transmission=0&bodySize=5&trim=0&paintId=imagin-grey&angle=23";
-  const element = `
+  // Fetch offers
+  fetch(
+    "https://cdn.jsdelivr.net/gh/965311532/genoleggia.it@dev/assets/offers.json"
+  ).then((res) =>
+    res.json().then((data) => {
+      console.debug("Offers: " + data);
+      // Get the container element
+      const target = getElementById("hero-offers");
+      // Clear the container
+      target.innerHTML = "";
+      // Add the offers (first 3)
+      for (let i = 0; i < 3; i++) {
+        target.innerHTML += HeroCarOffer(data[i]);
+      }
+    })
+  );
+}
+
+function HeroCarOffer(props) {
+  return `
   <div class="car-offer">
-    <h3 class="car-offer-price">263,00€</h3>
+    <h3 class="car-offer-price">€${parseInt(props.price).toFixed(0)},00€</h3>
     <div class="car-price-details">MESE I.E.</div>
     <div class="car-offer-image">
-      <img src=${imageUrl} alt="" class="image-3">
+      <img src=${props.image} alt="" class="image-3">
     </div>
-    <h3 car-name="" class="car-offer-price smaller">Lancia Ypsilon</h3>
-    <div class="car-price-details">Anticipo €0 · 48 mesi · 10.000 km/anno</div>
+    <h3 class="car-offer-price smaller">${props.name}</h3>
+    <div class="car-price-details">${props.details}</div>
     <div class="car-offer-btn">
-      <a href="/lancia-ypsilon" class="button w-button">Richiedi informazioni</a>
+      <a href="#" class="button w-button">Richiedi informazioni</a>
     </div>
   </div>`;
-  console.debug("Element: " + element);
-  target.innerHTML = element;
 }
