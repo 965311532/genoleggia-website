e3a8a065e9daf812858915b2a6ea195096df8e50
Gabriele Armento <gabriele.armento@gmail.com>
2023-06-01 17:16:27 +0200

    Add location page functionality

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ function start() {
   switch (currentPath) {
     case "/":
       loadHomePageOffers();
+      fixLocationForm();
       break;
     case "/offers":
       loadOffersDetails();
+      break;
+    case "locations":
+      loadLocations();
       break;
     default:
       break;
@@ -32,6 +36,12 @@ function getElementById(id) {
   }
 }
 
+// Function to sleep for a given amount of time
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+// Helper function to get the offers
 async function getOffers() {
   // Return the offers store in the assets folder
   console.debug("Fetching offers...");
@@ -69,6 +79,7 @@ function loadHomePageOffers() {
   });
 }
 
+// Car offer string template for the hero section
 function HeroCarOffer(props) {
   return `
   <div class="car-offer">
@@ -85,6 +96,7 @@ function HeroCarOffer(props) {
   </div>`;
 }
 
+// Car offer string template for the other offers section
 function CarOffer(props) {
   return `
   <div class="car-offer">
@@ -122,6 +134,7 @@ function loadOffersDetails() {
   });
 }
 
+// Offer details string template
 function OfferDetails(props) {
   return `
   <div class="car-details-image-wrapper">
@@ -130,9 +143,101 @@ function OfferDetails(props) {
   <div class="car-details-info">
     <h3 class="title red bigger">${props.name}</h3>
     <div class="subtitle">${props.details}</div>
-    <h1 class="title red bold">${parseFloat(props.price).toFixed(0)},00€</h1>
+    <h1 class="title red bold">${parseFloat(props.price).toFixed(0)},00€ 
+      <span class="text-span">mese i.e.</span>
+    </h1>
     <div class="car-details-button">
       <a href="#contact-us-form" class="button-hero red w-button">Richiedi preventivo</a>
     </div>
   </div>`;
+}
+
+// Function to load the regions
+async function getRegions() {
+  console.debug("Fetching regions...");
+  return fetch(
+    "https://cdn.jsdelivr.net/gh/965311532/genoleggia.it@4bd6c6bed1aa46a7ea02b6647bb0982a496e8707/assets/regions.json"
+  ).then((res) => res.json());
+}
+
+// Function to fix the location form
+function fixLocationForm() {
+  // Get the `locations` select element
+  const locationsSelect = getElementById("locations");
+  getRegions().then((regions) => {
+    // Add the regions to the select element
+    locationsSelect.innerHTML = "";
+    regions.forEach((region) => {
+      locationsSelect.innerHTML += `<option value="${region.name}">${region.name}</option>`;
+    });
+  });
+  // When the `location-form` is submitted we want to intercept the event and redirect the user to the correct location page
+  const locationForm = getElementById("location-form");
+  locationForm.addEventListener("submit", (event) => {
+    // Prevent the default form submission
+    event.preventDefault();
+    // Get the selected region
+    const selectedRegion = locationsSelect.value;
+    // Redirect the user to the correct location page
+    window.location.href = `/locations?region=${selectedRegion}`;
+  });
+}
+
+// Function to load the locations
+async function getLocations() {
+  console.debug("Fetching locations...");
+  return fetch(
+    "https://cdn.jsdelivr.net/gh/965311532/genoleggia.it@4bd6c6bed1aa46a7ea02b6647bb0982a496e8707/assets/locations.json"
+  ).then((res) => res.json());
+}
+
+// Function to load the locations page
+function loadLocationsPage() {
+  // Get the query string
+  const queryString = window.location.search;
+  console.debug("Query string: " + queryString);
+  // Get the query params
+  const urlParams = new URLSearchParams(queryString);
+  // Get the region
+  const region = urlParams.get("region");
+  console.debug("Region: " + region);
+  // Get the title, image and blocks container elements
+  const title = getElementById("location-title");
+  const image = getElementById("location-image");
+  const blocks = getElementById("location-blocks");
+  // Set the title
+  title.innerHTML = region;
+  // Set the image
+  getRegions().then((regions) => {
+    const regionObj = regions.find((r) => r.name === region);
+    image.src = regionObj.image;
+  });
+  // Set the location blocks
+  getLocations().then((locations) => {
+    blocks.innerHTML = "";
+    const regionLocations = locations[region];
+    regionLocations.forEach((location, index) => {
+      blocks.innerHTML += LocationBlock(
+        location,
+        index === regionLocations.length - 1
+      );
+    });
+  });
+}
+
+// Location block string template
+function LocationBlock(props, last) {
+  return `
+<div class="location-block">
+  <div class="location-info">
+    <strong>${props.name}<br></strong>${props.address}<br>${props?.tel}${
+    props?.tel && props?.email ? " • " : ""
+  }${props?.email}
+  </div>
+  <a href="${
+    props?.email ? `mailto:${props.email}` : `tel:${props.tel}`
+  }" class="button-hero red w-button">Contatta subito</a>
+  <div class="${last ? "hidden" : "location-divider"}"></div>  
+</div>
+`;
 }
